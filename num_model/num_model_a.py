f3fd1820a002ce236fc7a2b20d88e2d2e5f9aad2
enekorodriguez <rodriguezplazaeneko@gmail.com>
2020-03-06 13:43:31 +0100

    assym mod

--- num_model/num_model_a.py
+++ num_model/num_model_a.py
@@ -5,13 +5,14 @@ Created on Fri Mar  6 11:12:06 2020
 @author: rodri
 """
 
+
 import control as c
 import complete_par as par
 import numpy as np
 import matplotlib.pyplot as plt
 
 C1 = np.matrix([[(par.CYbdot-2*par.mub)*par.b/par.V0, 0, 0, 0],
-                [0, -par.b/2*par.V0, 0, 0],
+                [0, -par.b/(2*par.V0), 0, 0],
                 [0,0, -2*par.mub*par.KX2*(par.b**2)/(par.V0**2), 2*par.mub*par.KXZ*(par.b**2)/(par.V0**2)],
                 [par.Cnbdot*(par.b**2)/(par.V0**2), 0, 2*par.mub*par.KXZ*(par.b**2)/(par.V0**2), -2*par.mub*par.KZ2* (par.b**2)/(par.V0**2)]])
 C2 = - np.matrix([[par.CYb, par.CL, par.CYp*(par.b)/(2*par.V0), (par.CYr-4*par.mub)*(par.b)/(2*par.V0)],
@@ -30,21 +31,24 @@ C2_inv = np.linalg.inv(C2)
 A = - np.matmul(C1_inv, C2)
 B = - np.matmul(C2_inv, C3)
 
-
+print(B)
 
 C = np.matrix([[1, 0, 0, 0],
                [0, 1, 0, 0],
                [0, 0, 1, 0],
                [0, 0, 0, 1]])
-D = np.matrix([[0], [0], [0], [0]])
+D = np.matrix([[0, 0], [0, 0], [0, 0], [0, 0]])
 
-t = np.linspace(0,5, 500)
-
+t_sim = 120
+dt_sim = 0.05
+num_dts = int(t_sim/dt_sim)
+t = np.linspace(0,t_sim, num_dts)
 sys = c.ss(A,B,C,D)
+print(sys)
 
 sys_response = c.impulse_response(sys, t)
-
-print('u', sys_response[1][0][-1],'a', sys_response[1][1][-1],'t', sys_response[1][2][-1],'q', sys_response[1][3][-1],)
-
-plt.plot(sys_response[0], sys_response[1][1])
+# print('u', sys_response[1][0][-1],'a', sys_response[1][1][-1],'t', sys_response[1][2][-1],'q', sys_response[1][3][-1],)
+print(sys_response[1][1])
+plt.plot(sys_response[0][100:], sys_response[1][1][100:])
 plt.show()
+print(sys_response[1][0][0])
