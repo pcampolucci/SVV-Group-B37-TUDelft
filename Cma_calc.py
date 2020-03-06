7bce5ab0b4f9b5b7bdae9c80e9622aaacda7c6a8
Marek Travnik <marek.travnik.98@gmail.com>
2020-03-06 17:37:45 +0100

    Cma Cmd

---
+++ Cma_calc.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from Cmd_calc import Cmd
+
+alphas = np.array([5.3,6.3,7.3,8.5,4.5,4.1,3.4])
+
+deltas = np.array([0,-0.4,-0.9,-1.5,0.4,0.6,1])
+
+A = np.vstack([alphas, np.ones(len(alphas))]).T
+c1, c2 = np.linalg.lstsq(A, deltas, rcond=None)[0]
+
+
+Cma = -c1*Cmd
+plt.scatter(alphas,deltas)
+plt.show()
+
+print("Cma is:", Cma)
--- Cmd_calc.py
+++ Cmd_calc.py
@@ -3,8 +3,8 @@ from clcd_clalpha_plots import S,c
 
 moved_pax = 'Seat 7'    #which passenger was moved?
 moved_to  =  134        #to which position?
-V_m       =
-rho       =
+VEAS      =  82.6843    # [m/s]
+rho_0       =  1.225      # [kg/m3]
 
 #first measurement
 de1 = 0
@@ -13,16 +13,21 @@ t1  = 51*60 + 2
 de2 = -0.5
 t2  = 52*60 + 46
 
-dde = de2-de1
+dde = (de2-de1)*2*np.pi/360
 
+update_fuel_balance(t1)
 xcg1 = components['TM'].xcg()
 
 components[moved_pax].xcg_ = inches_to_m(moved_to)
-
+update_fuel_balance(t2)
 xcg2 = components['TM'].xcg()
 
-W = 
 dxcg = xcg2-xcg1
-CN = W/(1/2*rho*V_m**2*S)
+W = components['TM'].weight()
+
+CN = W/(1/2*rho_0*VEAS**2*S)
 Cmd = -1/dde * CN * dxcg / c
+
+print("Cn is: ", CN)
+print("Cmd is:", Cmd)
 
