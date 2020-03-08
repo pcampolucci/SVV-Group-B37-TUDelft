1c0385c4a04695cff8d47792c9e0d23a19fd64b2
Marco <marcodesiderio.1.0.0@gmail.com>
2020-03-08 13:38:35 +0100

    included case = sym + case = asym

--- num_model_symm/num_model_symm.py
+++ num_model_symm/num_model_symm.py
@@ -3,51 +3,73 @@ import complete_par as par
 import numpy as np
 import matplotlib.pyplot as plt
 
-#check if output makes sense
-#for a impulse everything explodes and then goes back to 0
-#velocity should not go back to 0
-#aoa, pitch and pitch rate make sense to go to 0
+#check if the results make sense and if feedback is needed.
 
-C1 = np.matrix([[-2*par.muc*par.c/(par.V0**2), 0, 0, 0],
-                [0, (par.CZadot - 2*par.muc)*par.c/par.V0, 0, 0],
-                [0,0, -par.c/par.V0, 0],
-                [0, par.Cmadot*par.c/par.V0, 0, -2*par.muc*par.KY2*(par.c/par.V0)**2]])
-C2 = - np.matrix([[-par.CXu/par.V0, -par.CXa, -par.CZ0, 0],
-                  [-par.CZu/par.V0, -par.CZa, par.CX0, (-par.CZq - 2*par.muc)*(par.c/par.V0)],
-                  [0, 0, 0, -(par.c/par.V0)],
-                  [-par.Cmu/par.V0, -par.Cma, 0, -par.Cmq*par.c/par.V0]])
-C3 = - np.matrix([[-par.CXde],
-                  [-par.CZde],
-                  [0],
-                  [-par.Cmde]])
+case = 'a'+'symmetric'
+if case == 'symmetric':
+    C1 = np.matrix([[-2*par.muc*par.c/(par.V0**2), 0, 0, 0],
+                    [0, (par.CZadot - 2*par.muc)*par.c/par.V0, 0, 0],
+                    [0,0, -par.c/par.V0, 0],
+                    [0, par.Cmadot*par.c/par.V0, 0, -2*par.muc*par.KY2*(par.c/par.V0)**2]])
+    C2 = - np.matrix([[-par.CXu/par.V0, -par.CXa, -par.CZ0, 0],
+                      [-par.CZu/par.V0, -par.CZa, par.CX0, (-par.CZq - 2*par.muc)*(par.c/par.V0)],
+                      [0, 0, 0, -(par.c/par.V0)],
+                      [-par.Cmu/par.V0, -par.Cma, 0, -par.Cmq*par.c/par.V0]])
+    C3 = - np.matrix([[-par.CXde],
+                      [-par.CZde],
+                      [0],
+                      [-par.Cmde]])
+elif case == 'asymmetric':
+    C1 = np.matrix([[(par.CYbdot - 2 * par.mub) * par.b / par.V0, 0, 0, 0],
+                    [0, -par.b / (2 * par.V0), 0, 0],
+                    [0, 0, -2 * par.mub * par.KX2 * (par.b ** 2) / (par.V0 ** 2),
+                     2 * par.mub * par.KXZ * (par.b ** 2) / (par.V0 ** 2)],
+                    [par.Cnbdot * (par.b) / (par.V0), 0, 2 * par.mub * par.KXZ * (par.b ** 2) / (par.V0 ** 2),
+                     -2 * par.mub * par.KZ2 * (par.b ** 2) / (par.V0 ** 2)]])
+    C2 = np.matrix([[par.CYb, par.CL, par.CYp * (par.b) / (2 * par.V0), (par.CYr - 4 * par.mub) * (par.b) / (2 * par.V0)],
+                    [0, 0, 1, 0],
+                    [par.Clb, 0, par.Clp * (par.b) / (2 * par.V0), par.Clr * (par.b) / (2 * par.V0)],
+                    [par.Cnb, 0, par.Cnp * (par.b) / (2 * par.V0), par.Cnr * (par.b) / (2 * par.V0)]])
+    C3 = np.matrix([[par.CYda, par.CYdr],
+                    [0, 0],
+                    [par.Clda, par.Cldr],
+                    [par.Cnda, par.Cndr]])
 
-# print(C1)
-# print(C2)
-# print(C3)
+else:
+    print('Case must be either symmetric or asymmetric')
+
 C1_inv = np.linalg.inv(C1)
 C2_inv = np.linalg.inv(C2)
 A = - np.matmul(C1_inv, C2)
 B = - np.matmul(C1_inv, C3)
-print(A)
-print(B)
-C = np.matrix([[1, 0, 0, 0],
-               [0, 1, 0, 0],
-               [0, 0, 1, 0],
-               [0, 0, 0, 1]])
-D = np.matrix([[0], [0], [0], [0]])
+
+#outputs
+if case == 'symmetric':
+    C = np.matrix([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+    D = np.matrix([[0], [0], [0], [0]])
+elif case == 'asymmetric':
+    C = np.matrix([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, 1, 0],
+                   [0, 0, 0, 1]])
+    D = np.matrix([[0, 0], [0, 0], [0, 0], [0, 0]])
+else:
+    print('Case must be either symmetric or asymmetric')
+#define simulation time and time steps
 t_sim = 120
 dt_sim = 0.05
 num_dts = int(t_sim/dt_sim)
 dts = dt_sim*np.ones(num_dts)
 t = np.linspace(0,t_sim, num_dts)
+
+#create state space linear system and compute system response
 sys = c.ss(A,B,C,D)
-print(sys)
 sys_response = c.impulse_response(sys, t)
-# print('u', sys_response[1][0][-1],'a', sys_response[1][1][-1],'t', sys_response[1][2][-1],'q', sys_response[1][3][-1],)
-print(sys_response[1][1])
-# Vs = sys_response[1][0]/np.cos(sys_response[1][2])
-# v = sys_response[1][0]*np.tan(sys_response[1][2])
-# h = par.hp0 + np.cumsum(v*dts)
+
+#plotting stuff
 plt.plot(sys_response[0], sys_response[1][0])
 plt.show()
 print(sys_response[1][0][0])
