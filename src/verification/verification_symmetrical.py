2acfe76d4c9c36311ed4a6b50e5674240a62e0f7
pietro campolucci <piecampo98@gmail.com>
2020-03-12 14:51:21 +0100

    fixed repo

--- src/verification/verification_symmetrical.py
+++ src/verification/verification_symmetrical.py
@@ -1,5 +1,5 @@
 """
-Title: Verification script for asymmetrical maneuvers. It will cover:
+Title: verification script for asymmetrical maneuvers. It will cover:
        - Short period motion 
        - Phugoid motion
 
